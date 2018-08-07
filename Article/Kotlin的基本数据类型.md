5e32ee28cb1a5e2fc3811b71e2472fc8cce3ced7
Fizzer_Mac <doraemonma503@gmail.com>
2018-08-07 11:02:51 +0800

    first Commit

---
+++ Article/Kotlin的基本数据类型.md
@@ -0,0 +1,4 @@
+##Kotlin入门----什么是Kotlin
+
+ Kotlin是一门可以运行在java虚拟机、Android、浏览器上的静态语言，与
+ Java100%的兼容
---
+++ src/Chapter1/main.kt
@@ -0,0 +1,14 @@
+package Chapter1
+
+/**
+ * Created by Doraemon on 2018/8/7.
+ * Kotlin学习基础
+ */
+
+fun main(args: Array<String>) {
+    printStr("Fizzer")
+}
+
+fun printStr(str: String){
+    println(str)
+}
